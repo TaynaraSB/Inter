990e018b73cd90ee974fc2743d55c4700b5198d4
tberger <taynarast.b@gmail.com>
2019-06-08 23:45:32 -0300

    FALTA BOTAS MENOS e VISUALIZAR SALAO+FOTO

--- app/src/main/java/com/br/opet/inter/NovoSalao.java
+++ app/src/main/java/com/br/opet/inter/NovoSalao.java
@@ -11,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
